17fd16c7ba23a2e9493745fa005b68b53bc52a1f
speeDy167 <trongphucphan7@gmail.com>
2022-03-15 22:56:13 +0700

    first commit

--- MaNguon/CMPtime.cpp
+++ MaNguon/CMPtime.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#define _for(i,a,b) for(int i=(a),_b=(b);i<_b;++i)
 
 using namespace std;
 
@@ -14,14 +13,14 @@ string to_str(int val)
     reverse(q.begin(), q.end());
     return q;
 }
-void chambai(string name, vector<double> &data)
+void Do(string name, vector<double> &data)
 {
-    cout<<"Judging "<<name<<"!!!!!!!!!!!!\n";
+    cout<<"Loading "<<name<<"!!!\n";
     clock_t start, end;
     double time_use;
     for(int i = 1; i<=10; ++i)
     {
-        cout<<"Running on test "<<i<<":\n";
+        cout<<"Running Test "<<i<<":\n";
         start = clock();
         system((name + ".exe " + to_str(i)).c_str());
         end = clock();
@@ -29,45 +28,38 @@ void chambai(string name, vector<double> &data)
         cout<<name<<"\'s running time on test "<<i<<" : "<<time_use<<"\n";
         data.push_back(time_use);
     }
-    cout<<"Finished judging!!!!!!\n";
+    cout<<"Hoan thanh"<< endl;
 }
 int main()
 {
     system("Test.exe");
-    cout<<"HOAN THANH SINH TEST!!!!!\n";
+    cout<<"HOAN THANH SINH TEST!!!\n";
     vector<double> d1,d2,d3,d4;
-    ///check thoi gian chay heapsort
-    chambai("heapsort", d1);
-    ///check thoi gian chay mergesort
-    chambai("mergesort", d2);
-    ///check thoi gian chay quicksort
-    chambai("quicksort", d3);
-    ///check thoi gian chay sort_std
-    chambai("sort_std", d4);
+    Do("heapsort", d1);
+    Do("mergesort", d2);
+    Do("quicksort", d3);
+    Do("sort_std", d4);
 
-
-    cout<<"BANG THOI GIAN SO SANH CAC THUAT TOAN SORT\n";
-    cout<<"##########################################\n";
-    cout<<setw(12)<<"thuat toan";
-    _for(i,1,11) cout<<setw(12)<<"test " + to_str(i);
+    cout<<"THOI GIAN CAC THUAT TOAN SORT\n";
+    cout<<setw(12)<<"Thuat toan";
+    for(int i=1;i<11;i++) cout<<setw(12)<<"test " + to_str(i);
     cout<<"\n";
     cout<<setw(12)<<"heapsort";
-    _for(i,0,10) cout<<setw(12)<<d1[i];
+    for(int i=0;i<10;++i) cout<<setw(12)<<d1[i];
     cout<<"\n";
 
     cout<<setw(12)<<"mergesort";
-    _for(i,0,10) cout<<setw(12)<<d2[i];
+    for(int i=0;i<10;++i) cout<<setw(12)<<d2[i];
     cout<<"\n";
 
     cout<<setw(12)<<"quicksort";
-    _for(i,0,10) cout<<setw(12)<<d3[i];
+    for(int i=0;i<10;++i) cout<<setw(12)<<d3[i];
     cout<<"\n";
 
     cout<<setw(12)<<"sort_std";
-    _for(i,0,10) cout<<setw(12)<<d4[i];
+    for(int i=0;i<10;++i) cout<<setw(12)<<d4[i];
     cout<<"\n";
     system("pause");
-
     return 0;
 }
 
